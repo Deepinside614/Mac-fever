7f3db35834d5477723acc0b0c3c908dc43dd67de
Deepinside614 <deepinside614@gmail.com>
2024-07-18 13:07:07 -0600

    Update Denial_code.py

--- Denial_code.py
+++ Denial_code.py
@@ -1,9 +1,9 @@
 import random
 import shutil
 
-def write_file(file_name, line_length=1000):
+def write_file(file_name, line_length=1000): # modify the line length to generate number of lines needed.
     with open(file_name, 'w') as file:
-        for i in range(10000):
+        for i in range(10000): # modify the number to generate the number of characters needed in a single line of the file.
             for x in range(line_length):
                 line_content = str(random.randint(0, 1))
                 file.write(''+line_content)
@@ -18,7 +18,7 @@ def main():
     newfile = "/users/USERNAME/desktop/test"
     # set the 'USERNAME' above to whatever the user you are logged in on for mac
     write_file(file)
-    multiply(file, newfile, 100000000000000000000000000000000) 
+    multiply(file, newfile, 100000000000000000000000000000000) # modify this number to modify the number of copies generated.
 
 if __name__ == "__main__":
     main()
